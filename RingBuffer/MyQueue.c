8cc8bee5bb81e72822dde45561f523f40cf21871
Hwi Yeon Cho <hwyn.cho@gmail.com>
2017-11-23 16:09:39 +0900

    Modify RingBuffer

--- RingBuffer/MyQueue.c
+++ RingBuffer/MyQueue.c
@@ -5,84 +5,84 @@
 
 void init(void)
 {
-	Queue = (int *)malloc(sizeof(int) * MAX_BUF_NUM);
-	memset(Queue, -1, sizeof(int) * MAX_BUF_NUM);
+    Queue = (int *)malloc(sizeof(int) * MAX_BUF_NUM);
+    memset(Queue, -1, sizeof(int) * MAX_BUF_NUM);
 
-	front = 0;
-	rear = -1;
+    front = 0;
+    rear = -1;
 }
 
 void enQueue(const int data)
 {
-	printf("<< enQueue(%d)", data);
+    printf("<< enQueue(%d)", data);
 
-	if (isFull() == 1)
-	{
-		puts(": Queue is full!");
-		deQueue();
-	}
-	else
-	{
-		rear = (rear + 1) % MAX_BUF_NUM;
-		Queue[rear] = data;
-		puts("");
-	}
+    if (isFull() == 1)
+    {
+        puts(": Queue is full!");
+        deQueue();
+    }
+    else
+    {
+        rear = (rear + 1) % MAX_BUF_NUM;
+        Queue[rear] = data;
+        puts("");
+    }
 }
 
 void deQueue(void)
 {
-	printf(">> deQueue()");
+    printf(">> deQueue()");
 
-	if (isEmpty() == 1)
-	{
-		puts(": Queue is empty!");
-	}
-	else
-	{
-		Queue[front] = -1;
-		front = (front + 1) % MAX_BUF_NUM;
-		puts("");
-	}
+    if (isEmpty() == 1)
+    {
+        puts(": Queue is empty!");
+    }
+    else
+    {
+        Queue[front] = -1;
+        front = (front + 1) % MAX_BUF_NUM;
+        puts("");
+    }
 }
 
 void clearQueue(void)
 {
-	free(Queue);
+    free(Queue);
 }
 
 void printQueue(void)
 {
-	int	i = 0;
-	int _front = front % MAX_BUF_NUM;
-	int _rear = rear % MAX_BUF_NUM;
+    int    i = 0;
+    int _front = front % MAX_BUF_NUM;
+    int _rear = rear % MAX_BUF_NUM;
 
-	printf(">> Buffer Status %d %d: [", _front, _rear);
+    printf(">> Buffer Status %d %d: [", _front, _rear);
 
-	i = _front;
-	while (i != _rear)
-	{
-		printf("%3d ", Queue[i]);
-		i = (i + 1) % MAX_BUF_NUM;
-	}
-	printf("%3d ] \n", Queue[i]);
+    i = _front;
+    while (i != _rear)
+    {
+        printf("%3d ", Queue[i]);
+        i = (i + 1) % MAX_BUF_NUM;
+    }
+    printf("%3d ] \n", Queue[i]);
 }
 
 int isEmpty(void)
 {
-	if (front == rear)
-	{
-		return 1;
-	}
+    if (front == rear)
+    {
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
 
 int isFull(void)
 {
-	if ((rear != -1) && ((rear + 1) % MAX_BUF_NUM) == front)
-	{
-		return 1;
-	}
+    if ((rear != -1) && ((rear + 1) % MAX_BUF_NUM) == front)
+    {
+        return 1;
+    }
 
-	return 0;
+    return 0;
 }
---
+++ RingBuffer/main.c
@@ -0,0 +1,100 @@
+#define _GNU_SOURCE
+#include <assert.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include "MyQueue.h"
+
+#define FLAGS (SIGCHLD | CLONE_VM | CLONE_SIGHAND | CLONE_FS | CLONE_FILES)
+#define STACK_SIZE    (1024 * 8)
+
+void testcase();
+int produce(void * arg);
+int consume(void * arg);
+
+pid_t pid_producer = 0;
+pid_t pid_consumer = 0;
+
+int produce_time = 0;
+int consume_time = 0;
+
+int main(int argc, char const * argv[]) {
+    int testcase_num = 0;
+    printf("<< Testcase 번호 입력 (1 or 2 or 3): ");
+    scanf("%d", &testcase_num);
+
+    switch (testcase_num) {
+        case 1:
+            produce_time = 3;
+            consume_time = 2;
+            break;
+
+        case 2:
+            produce_time = 2;
+            consume_time = 2;
+            break;
+
+        case 3:
+            produce_time = 2;
+            consume_time = 3;
+            break;
+
+        default:
+            puts("잘못된 입력값 입니다.");
+            return -1;
+    }
+
+    srand(time(NULL));
+    init();
+    testcase();
+
+    while (1)
+    {
+        sleep(produce_time + consume_time);
+        printQueue();
+    }
+
+    return 0;
+}
+
+void testcase()
+{
+    char * pStack_producer = (char *)malloc(sizeof(char) * STACK_SIZE);
+    char * pStack_consumer = (char *)malloc(sizeof(char) * STACK_SIZE);
+
+    pid_producer = clone(produce, (char *)pStack_producer + STACK_SIZE, FLAGS, &produce_time);
+    pid_consumer = clone(consume, (char *)pStack_consumer + STACK_SIZE, FLAGS, &consume_time);
+}
+
+int produce(void * arg)
+{
+    unsigned int * sleep_time = arg;
+
+    while (1)
+    {
+        enQueue(rand() % 100 + 1);
+        sleep(*sleep_time);
+    }
+
+    return 0;
+}
+
+int consume(void * arg)
+{
+    unsigned int * sleep_time = arg;
+
+    while (1)
+    {
+        deQueue();
+        sleep(*sleep_time);
+    }
+
+    return 0;
+}
