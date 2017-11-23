b534b9d77f12676244a5dc023a3a606b7b3dc4c0
Hwi Yeon Cho <hwyn.cho@gmail.com>
2017-11-23 16:15:22 +0900

    Modify StackCalculator

--- StackCalculator/StackCalculator.cpp
+++ StackCalculator/StackCalculator.cpp
@@ -6,7 +6,6 @@
  *
  * Last Updated : 2016/04/04 02:13
  */
-
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -22,232 +21,232 @@ void calculate(char(*_myOp)[10], const string & _postfix);
 
 int main(void)
 {
-	char myOp[3][10];
-	string expr, postfix;
+    char myOp[3][10];
+    string expr, postfix;
 
-	switch (loadOperators(myOp))
-	{
-	case -1:
-		cout << ">>> operators.txt does not exist! (Press ENTER key...)";
-		cin.get();
-		return 1;
+    switch (loadOperators(myOp))
+    {
+    case -1:
+        cout << ">>> operators.txt does not exist! (Press ENTER key...)";
+        cin.get();
+        return 1;
 
-	case 0:
-		while (true)
-		{
-			cout << "Input expressions : ";
-			cin >> expr;
+    case 0:
+        while (true)
+        {
+            cout << "Input expressions : ";
+            cin >> expr;
 
-			if (expr.compare("EOI") == 0)
-				return 0;
+            if (expr.compare("EOI") == 0)
+                return 0;
 
-			else
-			{
-				switch (checkBalance(expr))
-				{
-				case -1:
-					cout << ">>> Error!" << endl;
-					break;
+            else
+            {
+                switch (checkBalance(expr))
+                {
+                case -1:
+                    cout << ">>> Error!" << endl;
+                    break;
 
-				case 0:
-					postfix = makePostfix(expr);
-					cout << ">>> Postfix : " << postfix << endl;
-					calculate(myOp, postfix);
-				}
-			}
-		}
-	}
+                case 0:
+                    postfix = makePostfix(expr);
+                    cout << ">>> Postfix : " << postfix << endl;
+                    calculate(myOp, postfix);
+                }
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 int loadOperators(char(*_myOp)[10])
 {
-	ifstream file;
+    ifstream file;
 
-	file.open("operators.txt");
+    file.open("operators.txt");
 
-	if (file.is_open() == false)
-		return -1;
+    if (file.is_open() == false)
+        return -1;
 
-	else
-	{
-		for (unsigned int i = 0; i < 3; i++)
-		{
-			for (unsigned int j = 0; j < 10; j++)
-				file >> _myOp[i][j];
-		}
+    else
+    {
+        for (unsigned int i = 0; i < 3; i++)
+        {
+            for (unsigned int j = 0; j < 10; j++)
+                file >> _myOp[i][j];
+        }
 
-		file.close();
+        file.close();
 
-		return 0;
-	}
+        return 0;
+    }
 }
 
 int priority(char _op)
 {
-	switch (_op)
-	{
-	case '@': case '#':
-		return 0;
+    switch (_op)
+    {
+    case '@': case '#':
+        return 0;
 
-	case '&':	return 1;
+    case '&':    return 1;
 
-	case '(':	return 2;
+    case '(':    return 2;
 
-	default:	return -1;
-	}
+    default:    return -1;
+    }
 }
 
 int checkBalance(const string & _expr)
 {
-	int count = 0;
+    int count = 0;
 
-	for (unsigned int i = 0; i < _expr.length(); i++)
-	{
-		switch (_expr[i])
-		{
-		case '(':
-			count++;
-			break;
+    for (unsigned int i = 0; i < _expr.length(); i++)
+    {
+        switch (_expr[i])
+        {
+        case '(':
+            count++;
+            break;
 
-		case ')':
-			count--;
-			break;
-		}
-	}
+        case ')':
+            count--;
+            break;
+        }
+    }
 
-	if (count != 0)
-		return -1;
+    if (count != 0)
+        return -1;
 
-	else
-		return 0;
+    else
+        return 0;
 }
 
 string makePostfix(const string & _expr)
 {
-	unsigned int i = 0;
-	char temp;
-	string postfix;
-	stack<char> tempStack;
+    unsigned int i = 0;
+    char temp;
+    string postfix;
+    stack<char> tempStack;
 
-	while (i < _expr.length())
-	{
-		switch (_expr[i])
-		{
-		case 'a': case 'b': case 'c':
-			postfix += _expr[i++];
-			break;
+    while (i < _expr.length())
+    {
+        switch (_expr[i])
+        {
+        case 'a': case 'b': case 'c':
+            postfix += _expr[i++];
+            break;
 
-		case '@': case '#': case '&':
-			while (!tempStack.empty() && (tempStack.top() != '(') && (priority(tempStack.top()) >= priority(_expr[i])))
-			{
-				postfix += tempStack.top();
-				tempStack.pop();
-			}
+        case '@': case '#': case '&':
+            while (!tempStack.empty() && (tempStack.top() != '(') && (priority(tempStack.top()) >= priority(_expr[i])))
+            {
+                postfix += tempStack.top();
+                tempStack.pop();
+            }
 
-			tempStack.push(_expr[i++]);
-			break;
+            tempStack.push(_expr[i++]);
+            break;
 
-		case '(':
-			tempStack.push(_expr[i++]);
-			break;
+        case '(':
+            tempStack.push(_expr[i++]);
+            break;
 
-		case ')':
-			while (true)
-			{
-				temp = tempStack.top();
-				tempStack.pop();
+        case ')':
+            while (true)
+            {
+                temp = tempStack.top();
+                tempStack.pop();
 
-				if (temp == '(')
-					break;
+                if (temp == '(')
+                    break;
 
-				else
-					postfix += temp;
-			}
+                else
+                    postfix += temp;
+            }
 
-			i++;
-			break;
-		}
-	}
+            i++;
+            break;
+        }
+    }
 
-	while (!tempStack.empty())
-	{
-		postfix += tempStack.top();
-		tempStack.pop();
-	}
+    while (!tempStack.empty())
+    {
+        postfix += tempStack.top();
+        tempStack.pop();
+    }
 
-	return postfix;
+    return postfix;
 }
 
 void calculate(char(*_myOp)[10], const string & _postfix)
 {
-	unsigned int i = 0;
-	int name, result;
-	char left, right;
-	stack<char> tempStack;
+    unsigned int i = 0;
+    int name, result;
+    char left, right;
+    stack<char> tempStack;
 
-	while (i < _postfix.length())
-	{
-		switch (_postfix[i])
-		{
-		case 'a': case 'b': case 'c':
-			tempStack.push(_postfix[i++]);
-			break;
+    while (i < _postfix.length())
+    {
+        switch (_postfix[i])
+        {
+        case 'a': case 'b': case 'c':
+            tempStack.push(_postfix[i++]);
+            break;
 
-		case '@': case '#': case '&':
-			right = tempStack.top();
-			tempStack.pop();
+        case '@': case '#': case '&':
+            right = tempStack.top();
+            tempStack.pop();
 
-			left = tempStack.top();
-			tempStack.pop();
+            left = tempStack.top();
+            tempStack.pop();
 
-			switch (_postfix[i])
-			{
-			case '@':	name = 0;	break;
+            switch (_postfix[i])
+            {
+            case '@':    name = 0;    break;
 
-			case '#':	name = 1;	break;
+            case '#':    name = 1;    break;
 
-			case '&':	name = 2;	break;
-			}
+            case '&':    name = 2;    break;
+            }
 
-			switch (left)
-			{
-			case 'a':
-				switch (right)
-				{
-				case 'a':	result = 1;	break;
+            switch (left)
+            {
+            case 'a':
+                switch (right)
+                {
+                case 'a':    result = 1;    break;
 
-				case 'b':	result = 4; break;
+                case 'b':    result = 4; break;
 
-				case 'c':	result = 7;	break;
-				}
+                case 'c':    result = 7;    break;
+                }
 
-			case 'b':
-				switch (right)
-				{
-				case 'a':	result = 2;	break;
+            case 'b':
+                switch (right)
+                {
+                case 'a':    result = 2;    break;
 
-				case 'b':	result = 5; break;
+                case 'b':    result = 5; break;
 
-				case 'c':	result = 8;	break;
-				}
+                case 'c':    result = 8;    break;
+                }
 
-			case 'c':
-				switch (right)
-				{
-				case 'a':	result = 3;	break;
+            case 'c':
+                switch (right)
+                {
+                case 'a':    result = 3;    break;
 
-				case 'b':	result = 6; break;
+                case 'b':    result = 6; break;
 
-				case 'c':	result = 9;	break;
-				}
-			}
+                case 'c':    result = 9;    break;
+                }
+            }
 
-			tempStack.push(_myOp[name][result]);
-			i++;
-		}
-	}
+            tempStack.push(_myOp[name][result]);
+            i++;
+        }
+    }
 
-	cout << ">>> Result : " << tempStack.top() << endl;
+    cout << ">>> Result : " << tempStack.top() << endl;
 }
--- StackCalculator/StackCalculator_Double.cpp
+++ StackCalculator/StackCalculator_Double.cpp
@@ -6,7 +6,6 @@
  *
  * Last Updated : 2016/11/03 03:08
  */
-
 #include <iostream>
 #include <fstream>
 #include <stack>
@@ -18,275 +17,275 @@ using namespace std;
 class StackCalculator
 {
 private:
-	string formula;
-	vector<string> infix;
-	vector<string> postfix;
+    string formula;
+    vector<string> infix;
+    vector<string> postfix;
 
 public:
-	StackCalculator();
-	int priority(string op);
-	int checkBracket();
-	void setFormula(string input);
-	void setInfix();
-	void setPostfix();
-	double calculate();
+    StackCalculator();
+    int priority(string op);
+    int checkBracket();
+    void setFormula(string input);
+    void setInfix();
+    void setPostfix();
+    double calculate();
 };
 
 int main(void)
 {
-	string input;
-	StackCalculator calculator = StackCalculator();
+    string input;
+    StackCalculator calculator = StackCalculator();
 
-	while (true)
-	{
-		cout << "<<< Input : ";
-		input.clear();
-		cin >> input;
+    while (true)
+    {
+        cout << "<<< Input : ";
+        input.clear();
+        cin >> input;
 
-		if (input.compare("EOI") == 0)
-			return 0;
+        if (input.compare("EOI") == 0)
+            return 0;
 
-		else
-		{
-			calculator.setFormula(input);
+        else
+        {
+            calculator.setFormula(input);
 
-			switch (calculator.checkBracket())
-			{
-			case -1:
-				cout << ">>> Error!" << endl;
-				break;
+            switch (calculator.checkBracket())
+            {
+            case -1:
+                cout << ">>> Error!" << endl;
+                break;
 
-			case 0:
-				cout << ">>> Result : " << calculator.calculate() << endl;
-			}
-		}
-	}
+            case 0:
+                cout << ">>> Result : " << calculator.calculate() << endl;
+            }
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 StackCalculator::StackCalculator()
 {
-	formula.clear();
-	infix.clear();
-	postfix.clear();
+    formula.clear();
+    infix.clear();
+    postfix.clear();
 }
 
 int StackCalculator::priority(string op)
 {
-	if (op == "(")
-		return 2;
-	else if ((op == "*") || (op == "/"))
-		return 1;
-	else if ((op == "+") || (op == "-"))
-		return 0;
-	else
-		return -1;
+    if (op == "(")
+        return 2;
+    else if ((op == "*") || (op == "/"))
+        return 1;
+    else if ((op == "+") || (op == "-"))
+        return 0;
+    else
+        return -1;
 }
 
 int StackCalculator::checkBracket()
 {
-	int count = 0;
+    int count = 0;
 
-	for (unsigned int i = 0; i < formula.length(); i++)
-	{
-		switch (formula[i])
-		{
-		case '(':
-			count++;
-			break;
+    for (unsigned int i = 0; i < formula.length(); i++)
+    {
+        switch (formula[i])
+        {
+        case '(':
+            count++;
+            break;
 
-		case ')':
-			count--;
-			break;
-		}
-	}
+        case ')':
+            count--;
+            break;
+        }
+    }
 
-	if (count != 0)
-		return -1;
+    if (count != 0)
+        return -1;
 
-	else
-		return 0;
+    else
+        return 0;
 }
 
 void StackCalculator::setFormula(string input)
 {
-	formula.clear();
-	formula = input;
+    formula.clear();
+    formula = input;
 }
 
 void StackCalculator::setInfix() {
-	unsigned int i = 0;
+    unsigned int i = 0;
 
-	string temp;
-	temp.clear();
+    string temp;
+    temp.clear();
 
-	while (i < formula.length())
-	{
-		switch (formula.at(i)) {
-		case '(':
-			infix.push_back(temp);
-			infix.push_back("(");
-			temp.clear();
-			break;
+    while (i < formula.length())
+    {
+        switch (formula.at(i)) {
+        case '(':
+            infix.push_back(temp);
+            infix.push_back("(");
+            temp.clear();
+            break;
 
-		case ')':
-			infix.push_back(temp);
-			infix.push_back(")");
-			temp.clear();
-			break;
+        case ')':
+            infix.push_back(temp);
+            infix.push_back(")");
+            temp.clear();
+            break;
 
-		case '+':
-			infix.push_back(temp);
-			infix.push_back("+");
-			temp.clear();
-			break;
+        case '+':
+            infix.push_back(temp);
+            infix.push_back("+");
+            temp.clear();
+            break;
 
-		case '-':
-			infix.push_back(temp);
-			infix.push_back("-");
-			temp.clear();
-			break;
+        case '-':
+            infix.push_back(temp);
+            infix.push_back("-");
+            temp.clear();
+            break;
 
-		case '*':
-			infix.push_back(temp);
-			infix.push_back("*");
-			temp.clear();
-			break;
+        case '*':
+            infix.push_back(temp);
+            infix.push_back("*");
+            temp.clear();
+            break;
 
-		case '/':
-			infix.push_back(temp);
-			infix.push_back("/");
-			temp.clear();
-			break;
+        case '/':
+            infix.push_back(temp);
+            infix.push_back("/");
+            temp.clear();
+            break;
 
-		case '0': case '1': case '2':
-		case '3': case '4': case '5':
-		case '6': case '7': case '8':
-		case '9': case '.':
-			temp += formula.at(i);
-			break;
-		}
+        case '0': case '1': case '2':
+        case '3': case '4': case '5':
+        case '6': case '7': case '8':
+        case '9': case '.':
+            temp += formula.at(i);
+            break;
+        }
 
-		i++;
-	}
+        i++;
+    }
 
-	infix.push_back(temp);
+    infix.push_back(temp);
 }
 
 void StackCalculator::setPostfix()
 {
-	unsigned int i = 0;
-	string temp;
-	stack<string> tempStack;
+    unsigned int i = 0;
+    string temp;
+    stack<string> tempStack;
 
-	while (i < infix.size())
-	{
-		if (infix.at(i) == "")
-		{
-			i++;
-		}
-		else if (infix.at(i) == "(")
-		{
-			tempStack.push(infix.at(i++));
-		}
-		else if (infix.at(i) == ")")
-		{
-			while (true)
-			{
-				temp = tempStack.top();
-				tempStack.pop();
+    while (i < infix.size())
+    {
+        if (infix.at(i) == "")
+        {
+            i++;
+        }
+        else if (infix.at(i) == "(")
+        {
+            tempStack.push(infix.at(i++));
+        }
+        else if (infix.at(i) == ")")
+        {
+            while (true)
+            {
+                temp = tempStack.top();
+                tempStack.pop();
 
-				if (temp == "(")
-					break;
-				else
-					postfix.push_back(temp);
-			}
+                if (temp == "(")
+                    break;
+                else
+                    postfix.push_back(temp);
+            }
 
-			i++;
-		}
-		else if ((infix.at(i) == "+") || (infix.at(i) == "-") || (infix.at(i) == "*") || (infix.at(i) == "/"))
-		{
-			while (!tempStack.empty() && (tempStack.top() != "(") && (priority(tempStack.top()) >= priority(infix.at(i))))
-			{
-				postfix.push_back(tempStack.top());
-				tempStack.pop();
-			}
+            i++;
+        }
+        else if ((infix.at(i) == "+") || (infix.at(i) == "-") || (infix.at(i) == "*") || (infix.at(i) == "/"))
+        {
+            while (!tempStack.empty() && (tempStack.top() != "(") && (priority(tempStack.top()) >= priority(infix.at(i))))
+            {
+                postfix.push_back(tempStack.top());
+                tempStack.pop();
+            }
 
-			tempStack.push(infix.at(i++));
-		}
-		else
-		{
-			postfix.push_back(infix.at(i++));
-		}
-	}
+            tempStack.push(infix.at(i++));
+        }
+        else
+        {
+            postfix.push_back(infix.at(i++));
+        }
+    }
 
-	while (!tempStack.empty())
-	{
-		postfix.push_back(tempStack.top());
-		tempStack.pop();
-	}
+    while (!tempStack.empty())
+    {
+        postfix.push_back(tempStack.top());
+        tempStack.pop();
+    }
 }
 
 double StackCalculator::calculate()
 {
-	unsigned int i = 0;
-	double left = 0;
-	double right = 0;
-	stack<string> tempStack;
+    unsigned int i = 0;
+    double left = 0;
+    double right = 0;
+    stack<string> tempStack;
 
-	infix.clear();
-	postfix.clear();
+    infix.clear();
+    postfix.clear();
 
-	setInfix();
-	setPostfix();
+    setInfix();
+    setPostfix();
 
-	while (i < postfix.size())
-	{
-		if (postfix.at(i) == "+") {
-			right = stod(tempStack.top());
-			tempStack.pop();
+    while (i < postfix.size())
+    {
+        if (postfix.at(i) == "+") {
+            right = stod(tempStack.top());
+            tempStack.pop();
 
-			left = stod(tempStack.top());
-			tempStack.pop();
+            left = stod(tempStack.top());
+            tempStack.pop();
 
-			tempStack.push(to_string(left + right));
-		}
-		else if (postfix.at(i) == "-")
-		{
-			right = stod(tempStack.top());
-			tempStack.pop();
+            tempStack.push(to_string(left + right));
+        }
+        else if (postfix.at(i) == "-")
+        {
+            right = stod(tempStack.top());
+            tempStack.pop();
 
-			left = stod(tempStack.top());
-			tempStack.pop();
+            left = stod(tempStack.top());
+            tempStack.pop();
 
-			tempStack.push(to_string(left - right));
-		}
-		else if (postfix.at(i) == "*")
-		{
-			right = stod(tempStack.top());
-			tempStack.pop();
+            tempStack.push(to_string(left - right));
+        }
+        else if (postfix.at(i) == "*")
+        {
+            right = stod(tempStack.top());
+            tempStack.pop();
 
-			left = stod(tempStack.top());
-			tempStack.pop();
+            left = stod(tempStack.top());
+            tempStack.pop();
 
-			tempStack.push(to_string(left * right));
-		}
-		else if (postfix.at(i) == "/")
-		{
-			right = stod(tempStack.top());
-			tempStack.pop();
+            tempStack.push(to_string(left * right));
+        }
+        else if (postfix.at(i) == "/")
+        {
+            right = stod(tempStack.top());
+            tempStack.pop();
 
-			left = stod(tempStack.top());
-			tempStack.pop();
+            left = stod(tempStack.top());
+            tempStack.pop();
 
-			tempStack.push(to_string(left / right));
-		}
-		else
-			tempStack.push(postfix.at(i));
+            tempStack.push(to_string(left / right));
+        }
+        else
+            tempStack.push(postfix.at(i));
 
-		i++;
-	}
+        i++;
+    }
 
-	return stod(tempStack.top());
+    return stod(tempStack.top());
 }
